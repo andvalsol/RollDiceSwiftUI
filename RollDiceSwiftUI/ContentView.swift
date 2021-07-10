0e39a5af2f5b38d47b5d5c015ad9fc764affc4d0
Andrey Solera <andrey.valverde.solera@gmail.com>
2021-07-10 11:11:01 -0600

    Finished project

--- RollDiceSwiftUI/ContentView.swift
+++ RollDiceSwiftUI/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    Dicee(number: "1")
+                    Dicee(number: "2")
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    // Generate random numbers
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+    }
+}
+
+struct Dicee: View {
+    
+    let number: String
+    
+    var body: some View {
+        Image("dice\(number)").resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
     }
 }
